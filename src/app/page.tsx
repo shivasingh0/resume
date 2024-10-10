fad5bb84efdbbeabaab76ad2b380c08ce9541c65
shivasingh0 <shiva2003rajawat@gmail.com>
2024-10-11 02:12:05 +0530

    "Updated CSS classes in Page component"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,7 @@ export default function Page() {
         </div>
       </section>
       <section id="projects">
-        <div className="space-y-12 w-full py-12">
+        <div className="space-y-12 w-full lg:py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
@@ -198,7 +198,7 @@ export default function Page() {
       <section id="contact">
         <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full">
           <BlurFade delay={BLUR_FADE_DELAY * 16}>
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center mb-12 items-center">
               <Link
                 download={true}
                 href="/ShivMangal_Resume.pdf"
